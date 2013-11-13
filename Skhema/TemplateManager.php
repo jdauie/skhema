3a226969d592dada598b38bef3c8c89d20f59530
josh <josh@4c7d2f06-2dde-428e-803f-e3bd2a63197d>
2013-11-13 04:52:13 +0000

    Refactoring cache generation to a new class.

--- Skhema/TemplateManager.php
+++ Skhema/TemplateManager.php
@@ -2,36 +2,28 @@
 
 namespace Jacere;
 
-require_once('Util.php');
-require_once('Util/Stopwatch.php');
-
 class TemplateManager {
 	
-	const DEFAULT_TEMPLATE_EXT = 'tpl';
-	
+	const TEMPLATE_EXT = 'tpl';
 	const CACHE_FORMAT = '_%s.cache';
 	
 	private static $c_manager;
 	
 	private $m_templates;
-	private $m_extension;
 	private $m_cache;
-	private $m_dir;
 	
-	function __construct($dir, $extension, $forceUpdate) {
+	function __construct($dir, $forceUpdate) {
+		$this->m_cache = $dir.'/'.sprintf(self::CACHE_FORMAT, self::TEMPLATE_EXT);
 		
-		if ($extension === NULL) {
-			$extension = self::DEFAULT_TEMPLATE_EXT;
-		}
-		
-		$this->m_dir = $dir;
-		$this->m_extension = '.'.$extension;
-		$this->m_cache = sprintf(self::CACHE_FORMAT, $extension);
-		
-		self::$c_manager = $this;
 		if ($forceUpdate || !$this->Deserialize()) {
-			$this->UpdateTemplateCache();
+			$this->m_templates = TemplateGenerator::Create($dir);
 		}
+		self::$c_manager = $this;
+	}
+	
+	public static function Create($dir, $forceUpdate = false) {
+		$manager = new TemplateManager($dir, $forceUpdate);
+		return $manager;
 	}
 	
 	public static function GetTemplate($name) {
@@ -39,292 +31,6 @@ class TemplateManager {
 			return self::$c_manager->m_templates[$name];
 		}
 		return NULL;
-	}
-	
-	public static function Create($dir, $extension = NULL, $forceUpdate = false) {
-		$manager = new TemplateManager($dir, $extension, $forceUpdate);
-		return $manager;
-	}
-	
-	private function UpdateTemplateCache() {
-		
-		//$sw = Stopwatch::StartNew('UpdateTemplateCache');
-		{
-			$files = $this->LoadTemplateFiles();
-			//$sw->Save('load');
-			
-			$files = $this->TokenizeTemplateFiles($files);
-			//$sw->Save('tokenize');
-			
-			$this->ParseTokens($files);
-			//$sw->Save('parse');
-			
-			$this->TopoSort();
-			//$sw->Save('toposort');
-			
-			$this->Finalize();
-			//$sw->Save('finalize');
-			
-			$this->Serialize();
-			//$sw->Save('serialize');
-		}
-		//$sw->Stop();
-		//echo $sw;
-	}
-	
-	private function LoadTemplateFiles() {
-		// keep files separate, for debugging
-		$files = [];
-		foreach (glob($this->m_dir.'/*'.$this->m_extension) as $entry) {
-			$fileContents = file_get_contents($entry);
-			if ($fileContents !== false) {
-				$files[$entry] = $fileContents;
-			}
-		}
-		
-		return $files;
-	}
-	
-	private function TokenizeTemplateFiles($files) {
-		
-		TokenType::Init();
-		
-		$result = [];
-		foreach($files as $name => $str) {
-			// testing out whitespace removal/reduction
-			$str = preg_replace('/\s{2,}/', "\n", $str);
-			$split = preg_split("/(\{[^\{\}]+\})/", $str, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
-			
-			$tokenFormatBeginLength = strlen(TokenType::T_FORMAT_BEGIN);
-			$tokenFormatEndLength = strlen(TokenType::T_FORMAT_END);
-			
-			$tokens = [];
-			
-			foreach ($split as $value) {
-				if ($value[0] == TokenType::T_FORMAT_BEGIN) {
-					// regex does not currently verify min length
-					$symbol = $value[$tokenFormatBeginLength];
-					$tokenType = TokenType::GetTokenTypeForSymbol($symbol);
-					
-					if ($tokenType != NULL) {
-						if ($tokenType == TokenType::T_CLOSE) {
-							$tokens[] = TokenType::T_CLOSE;
-						}
-						else {
-							$tokenName = substr($value, $tokenFormatBeginLength + 1, -$tokenFormatEndLength);
-							$tokens[] = new NameToken($tokenType, $tokenName);
-						}
-					}
-					else {
-						$tokens[] = $value;
-					}
-				}
-				else {
-					$tokens[] = $value;
-				}
-			}
-			
-			if (count($tokens)) {
-				$result[$name] = $tokens;
-			}
-		}
-		return $result;
-	}
-	
-	private function ParseTokens($files) {
-		$templates = [];
-		$stack = [];
-		$node = NULL;
-		
-		foreach($files as $name => $tokens) {
-			foreach($tokens as $token) {
-				if (is_string($token)) {
-					if ($node != NULL) {
-						//$node->AddChild($token);
-						$node->m_children[] = $token;
-					}
-					/*else {
-						if (!$token instanceof TextToken) {
-							// something other than whitespace/template at the root level
-						}
-					}*/
-				}
-				else if (is_int($token)) {
-					if ($token === TokenType::T_CLOSE) {
-						$nodeType = $node->GetType();
-						if ($nodeType == TokenType::T_TEMPLATE || $nodeType == TokenType::T_SOURCE) {
-							// add to template list
-							$templateName = $node->GetName();
-							if ($nodeType == TokenType::T_SOURCE) {
-								// generate name for "anonymous" template (there will be something on the stack for this type)
-								$templateParent = end($stack);
-								while ($templateParent->HasParent()) {
-									$templateParent = $templateParent->GetParent();
-								}
-								$templateParentPrefix = $templateParent->GetName();
-								$templateName = TokenType::T_ANONYMOUS_TEMPLATE_PREFIX.$templateParentPrefix.TokenType::T_ANONYMOUS_TEMPLATE_DELIMITER.$templateName;
-							}
-							if (isset($templates[$templateName])) {
-								die('Duplicate template definition: '.$templateName);
-							}
-							$templates[$templateName] = new Template($node, $templateName);
-							
-							// templates don't have a parent, so check the nesting stack
-							$parent = NULL;
-							if (count($stack) > 0) {
-								// this is a nested template (replace with include token)
-								$parent = array_pop($stack);
-								$includeToken = new NameToken(TokenType::T_INCLUDE, $templateName);
-								//$parent->AddChild($includeToken);
-								$parent->m_children[] = $includeToken;
-							}
-							$node = $parent;
-						}
-						else {
-							$node = $node->GetParent();
-						}
-					}
-				}
-				else {
-					$tokenType = $token->GetType();
-					if ($tokenType == TokenType::T_TEMPLATE || $tokenType == TokenType::T_SOURCE) {
-						if ($node != NULL) {
-							// save current node if this is a nested definition
-							$stack[] = $node;
-						}
-						else {
-							if ($tokenType != TokenType::T_TEMPLATE) {
-								die('Only explicit templates allowed at root level');
-							}
-						}
-						// start new template node (with no parent)
-						$node = new Node($token);
-					}
-					else if (!TokenType::GetTokenTypeDef($tokenType)->SelfClosing) {
-						if ($node != NULL) {
-							// this is an self-closing tag, so start a new node now
-							//$child = new Node($token);
-							//$node->AddChild($child);
-							$child = new Node($token, $node);
-							$node->m_children[] = $child;
-							$node = $child;
-						}
-					}
-					else {
-						if ($node != NULL) {
-							//$node->AddChild($token);
-							$node->m_children[] = $token;
-						}
-					}
-				}
-			}
-		}
-		
-		$this->m_templates = $templates;
-	}
-	
-	private function TopoSort() {
-		$edges = [];
-		$s = [];
-		foreach ($this->m_templates as $templateName => $template) {
-			if ($template->HasDependencies()) {
-				foreach ($template->GetDependencies() as $dependency) {
-					if (!isset($edges[$dependency])) {
-						$edges[$dependency] = [];
-					}
-					$edges[$dependency][] = $templateName;
-				}
-			}
-			else {
-				$s[] = $template;
-			}
-		}
-		
-		$sorted = [];
-		while (!empty($s)) {
-			// shift/pop doesn't matter for correctness
-			$nTemplate = array_pop($s);
-			$n = $nTemplate->GetName();
-			$sorted[$n] = $nTemplate;
-			if (isset($edges[$n])) {
-				$parents = &$edges[$n];
-				while (count($parents) > 0) {
-					$m = array_pop($parents);
-					$mTemplate = $this->m_templates[$m];
-		      $dependenciesSorted = true;
-		      foreach ($mTemplate->GetDependencies() as $dependency) {
-		      	if (!isset($sorted[$dependency])) {
-		      		$dependenciesSorted = false;
-		      		break;
-		      	}
-		      }
-		      if ($dependenciesSorted) {
-		      	$s[] = $mTemplate;
-		      }
-				}
-			}
-		}
-		// count edges to check for cycle
-		$edgesRemaining = 0;
-		foreach ($edges as $parents) {
-			$edgesRemaining += count($parents);
-		}
-		if ($edgesRemaining != 0) {
-			var_dump($edges);
-			die('graph cycle');
-		}
-		
-		$this->m_templates = $sorted;
-	}
-	
-	private function Finalize() {
-		foreach ($this->m_templates as $template) {
-			$template->Finalize();
-		}
-	}
-	
-	private function Serialize() {
-		$path = $this->m_dir.'/'.$this->m_cache;
-		
-		if (true) {
-			// gzencode is faster for serializing, but slightly slower for deserializing
-			// needs more testing on various systems with increased template complexity
-			// * this may need to be an option
-			$data = serialize($this->m_templates);
-			file_put_contents($path, $data);
-		}
-		else {
-			// this could be a better option with good bytecode caching
-			ob_start();
-			
-			foreach ($this->m_templates as $template) {
-				$template->Dump();
-			}
-			
-			$output = ob_get_contents();
-			ob_end_clean();
-			
-			$uniqueId = uniqid();
-			$output = <<<EOT
-<?php
-
-namespace Jacere\TemplateCache {
-function DeserializeCachedTemplates() {
-return \Jacere\Deserialize_{$uniqueId}();
-}
-}
-
-namespace Jacere {
-function Deserialize_{$uniqueId}() {
-\$templates = [];
-{$output}
-return \$templates;
-}
-}
-?>
-EOT;
-			file_put_contents($path, $output);
-		}
 	}
 	
 	private function Deserialize() {
